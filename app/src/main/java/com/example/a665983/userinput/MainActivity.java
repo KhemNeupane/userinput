b16d9556d968ceeff39ad84696f20ec7863a8a36
665983 <neupaneanuj1@gmail.com>
2018-03-29 10:41:57 -0500

    ckvxci0jfxmbgfdog

--- app/src/main/java/com/example/a665983/userinput/MainActivity.java
+++ app/src/main/java/com/example/a665983/userinput/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.a665983.userinput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name, email;
+    int favoriteNumber;
+
+    EditText nameInput;
+    EditText emailInput;
+    EditText favoriteNumberInput;
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameInput =(EditText) findViewById(R.id.nameInput);
     }
 }
